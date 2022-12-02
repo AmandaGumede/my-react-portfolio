8354f4e6c88933d64ea7c06dbaa975901ac026b7
amagumed021 <amagumed021@student.wethinkcode.co.za>
2022-12-02 10:53:29 +0200

    Updated Style Sheet and layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import profile from './profile.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profile} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
